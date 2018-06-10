7825f86bf1c6a90f9a3b99fe14f36e248c9e15d5
Danilson Sanches <dsanches06@outlook.com>
2018-06-10 01:54:27 +0100

    projeto com novos recursos

--- SmartHome-F2/src/smarthome/App.java
+++ SmartHome-F2/src/smarthome/App.java
@@ -10,6 +10,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -19,28 +21,24 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
     
+    private BorderPane root;
+
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+
+        //inicializa a borderpane
+        this.root = new BorderPane();
+        //scene
+        Scene scene = new Scene(root, 880, 620);
+        scene.getStylesheets().add(this.getClass().getResource("styles/estilos.css").toExternalForm());
+
+        primaryStage.setTitle("Smart Home");
+        primaryStage.getIcons().add(new Image("smarthome/styles/favicon.png"));
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     /**
      * @param args the command line arguments
      */
